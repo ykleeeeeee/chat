e6ab5f88bcde3d7ecc3d869bd0413061eaca1fcc
Yklee <yklee523@gail.com>
2023-10-14 23:51:24 +0800

    done with r1.py

---
+++ r1.py
@@ -0,0 +1,33 @@
+#讀取檔案
+def read_file(filename):
+    lines = []
+    with open(filename, 'r', encoding='utf-8-sig') as f:
+        for line in f:
+            lines.append(line.strip())
+    return lines
+
+def convert(lines):
+    new = []
+    person = None
+    for line in lines:
+        if line == 'Allen':
+            person = 'Allen'
+            continue          #直接跳到下一個迴圈 (下方不執行)
+        elif line == 'Tom':
+            person = 'Tom'
+            continue
+        if person:
+            new.append(person + ': ' + line)
+    return new
+
+def write_file(filename, lines):
+    with open(filename, 'w') as f:
+        for line in lines:
+            f.write(line + '\n')
+
+def main():
+    lines = read_file('input.txt')
+    lines = convert(lines)
+    write_file('output.txt', lines)
+
+main()
